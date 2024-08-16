2643b5d5365e61b77a797d2053c568c2c4694fc2
ashutosh29599 <ashutosh29599@yahoo.co.in>
2024-08-16 17:24:28 -0500

    Add forgot password functionality - the reset email is printed in the console for now.

--- chaiheadq/settings.py
+++ chaiheadq/settings.py
@@ -145,3 +145,14 @@ LOGOUT_REDIRECT_URL = '/tweet/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("DJANGO_EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("DJANGO_EMAIL_PASSWORD")
+
+DEFAULT_FROM_EMAIL = os.environ.get("DJANGO_EMAIL_USER")
+SERVER_EMAIL = os.environ.get("DJANGO_EMAIL_USER")
